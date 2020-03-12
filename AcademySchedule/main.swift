238300a9c69e4afd4d23a87cd3bba93b0202be73
Albert Rayneer <rayneerqueiroz@icloud.com>
2020-03-12 08:45:26 -0300

    Initial Commit

---
+++ AcademySchedule/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AcademySchedule
+//
+//  Created by Albert Rayneer on 12/03/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
